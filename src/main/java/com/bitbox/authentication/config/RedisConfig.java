b9bb78988e63ef707d11d4235f3f0264ae962095
wakkpu <ho0524870@gmail.com>
2023-10-16 14:14:59 +0900

    [FIX] using Environment

--- src/main/java/com/bitbox/authentication/config/RedisConfig.java
+++ src/main/java/com/bitbox/authentication/config/RedisConfig.java
@@ -1,19 +1,24 @@
 package com.bitbox.authentication.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
+@EnableRedisRepositories
 public class RedisConfig {
 
-    @Value("${spring.redis.host}")
     private String host;
 
-    @Value("${spring.redis.port}")
     private int port;
+
+    public RedisConfig(Environment env) {
+        this.host = env.getProperty("spring.redis.host");
+        this.port = Integer.parseInt(env.getProperty("spring.redis.port"));
+    }
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
