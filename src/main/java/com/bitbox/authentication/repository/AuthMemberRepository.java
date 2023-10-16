5022defbb28f86c83020e6ed11594df8bf39e9c2
wakkpu <ho0524870@gmail.com>
2023-10-16 14:18:42 +0900

    [FIX] method name

--- src/main/java/com/bitbox/authentication/repository/AuthMemberRepository.java
+++ src/main/java/com/bitbox/authentication/repository/AuthMemberRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface AuthMemberRepository extends CrudRepository<AuthMember, String> {
-    Optional<AuthMember> findAuthMemberByMemberEmailAndDeletedIsFalse(String memberEmail);
+    Optional<AuthMember> findByMemberEmailAndDeletedIsFalse(String memberEmail);
 }
