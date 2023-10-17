61ab3eb97e831619267d4a2347d337f5aedc3323
wakkpu <ho0524870@gmail.com>
2023-10-17 21:05:45 +0900

    [FIX] redis config password

--- src/main/java/com/bitbox/authentication/config/RedisConfig.java
+++ src/main/java/com/bitbox/authentication/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
@@ -14,14 +15,20 @@ public class RedisConfig {
     private String host;
 
     private int port;
+    private String password;
 
     public RedisConfig(Environment env) {
         this.host = env.getProperty("spring.redis.host");
         this.port = Integer.parseInt(env.getProperty("spring.redis.port"));
+        this.password = env.getProperty("spring.redis.password");
     }
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPort(port);
+        redisStandaloneConfiguration.setPassword(password);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 }
