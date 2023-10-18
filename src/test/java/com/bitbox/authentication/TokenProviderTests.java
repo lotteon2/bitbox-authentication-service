e881558b1644abe756da8647cf03dbdf66a392c0
wakkpu <ho0524870@gmail.com>
2023-10-18 21:23:54 +0900

    [TEST] parse test

--- src/test/java/com/bitbox/authentication/TokenProviderTests.java
+++ src/test/java/com/bitbox/authentication/TokenProviderTests.java
@@ -4,6 +4,7 @@ import com.bitbox.authentication.util.JwtProvider;
 import io.github.bitbox.bitbox.enums.AuthorityType;
 import io.github.bitbox.bitbox.enums.TokenType;
 import io.github.bitbox.bitbox.jwt.JwtPayload;
+import io.jsonwebtoken.Claims;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -55,5 +56,24 @@ public class TokenProviderTests {
         logger.info(refreshToken);
 
         assert !refreshToken.isBlank();
+    }
+
+    @Test
+    @DisplayName("refresh token claims 확인")
+    void parseRefreshTokenTest() {
+        JwtPayload jwtPayload = JwtPayload.builder()
+                .classId(null)
+                .memberId("UUID")
+                .memberAuthority(AuthorityType.MANAGER)
+                .memberNickname("manager")
+                .build();
+
+        String refreshToken = jwtProvider.generateToken(System.currentTimeMillis(), TokenType.REFRESH, jwtPayload);
+
+        Claims claims = jwtProvider.parse(refreshToken);
+
+        assert claims.get("member_id", String.class).equals("UUID");
+        assert AuthorityType.valueOf((String) claims.get("member_authority")).equals(AuthorityType.MANAGER);
+        assert claims.get("class_id", Long.class) == null;
     }
 }
