ce9d477b79f5a304e8c14052b5cc02880ba4eb5a
wakkpu <ho0524870@gmail.com>
2023-10-18 21:23:09 +0900

    [FEAT] refresh

--- src/main/java/com/bitbox/authentication/controller/AuthController.java
+++ src/main/java/com/bitbox/authentication/controller/AuthController.java
@@ -5,16 +5,17 @@ import com.bitbox.authentication.dto.response.Tokens;
 import com.bitbox.authentication.entity.AuthAdmin;
 import com.bitbox.authentication.service.AuthService;
 import com.bitbox.authentication.service.JwtService;
+import io.github.bitbox.bitbox.enums.AuthorityType;
 import io.github.bitbox.bitbox.jwt.JwtPayload;
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
@@ -24,17 +25,48 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/admin")
-    public ResponseEntity<Tokens> localLogin(@Valid @RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<Map<String, String>> localLogin(@Valid @RequestBody LoginRequest loginRequest) {
         AuthAdmin authAdmin = authService.findAuthAdmin(loginRequest.getEmail(), loginRequest.getPassword());
 
-        Tokens tokens = jwtService.generateTokens(JwtPayload.builder()
+        JwtPayload jwtPayload = JwtPayload.builder()
                 .memberAuthority(authAdmin.getAdminAuthority())
                 .memberNickname(authAdmin.getAdminName())
                 .memberId(authAdmin.getAdminId())
                 .classId(null)
-                .build());
+                .build();
 
+        // TODO : REFACTORING
+        Tokens tokens = jwtService.generateTokens(jwtPayload);
+        Map<String, String> resultMap = new HashMap<>();
+        resultMap.put("authority", jwtPayload.getMemberAuthority().name());
+        resultMap.put("accessToken", tokens.getAccessToken());
 
-        return ResponseEntity.status(HttpStatus.OK).body(tokens);
+        return ResponseEntity.status(HttpStatus.OK)
+                .header("Set-Cookie",
+                        jwtService.refreshTokenCookie(tokens.getRefreshToken()).toString())
+                .body(resultMap);
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<Map<String, String>> refresh(@RequestHeader("Refreshtoken") String refreshToken) {
+        Claims claims = jwtService.parse(refreshToken);
+
+        JwtPayload jwtPayload = JwtPayload.builder()
+                .memberAuthority(jwtService.getMemberAuthority(claims))
+                .memberNickname(jwtService.getMemberNickname(claims))
+                .memberId(jwtService.getMemberId(claims))
+                .classId(jwtService.getClassId(claims))
+                .build();
+
+        // TODO : REFACTORING
+        Tokens tokens = jwtService.generateTokens(jwtPayload);
+        Map<String, String> resultMap = new HashMap<>();
+        resultMap.put("authority", jwtPayload.getMemberAuthority().name());
+        resultMap.put("accessToken", tokens.getAccessToken());
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .header("Set-Cookie",
+                        jwtService.refreshTokenCookie(tokens.getRefreshToken()).toString())
+                .body(resultMap);
     }
 }
