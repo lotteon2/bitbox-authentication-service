ae2842d81ddff5c8ede706567aeb8799d9995e06
wakkpu <ho0524870@gmail.com>
2023-10-18 21:22:04 +0900

    [FEAT] parse claims

--- src/main/java/com/bitbox/authentication/service/JwtService.java
+++ src/main/java/com/bitbox/authentication/service/JwtService.java
@@ -2,11 +2,16 @@ package com.bitbox.authentication.service;
 
 import com.bitbox.authentication.dto.response.Tokens;
 import com.bitbox.authentication.util.JwtProvider;
+import io.github.bitbox.bitbox.enums.AuthorityType;
 import io.github.bitbox.bitbox.enums.TokenType;
 import io.github.bitbox.bitbox.jwt.JwtPayload;
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Service;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 @Service
 @RequiredArgsConstructor
@@ -29,5 +34,25 @@ public class JwtService {
                 .sameSite("None")
 //                    .secure(true) // TODO : secure(true)시 https에서만 작동.
                 .build();
+    }
+
+    public Claims parse(String refreshToken) {
+        return jwtProvider.parse(refreshToken);
+    }
+
+    public String getMemberId(Claims claims) {
+        return claims.get("member_id", String.class);
+    }
+
+    public String getMemberNickname(Claims claims) {
+        return URLEncoder.encode(claims.get("member_nickname", String.class), StandardCharsets.UTF_8);
+    }
+
+    public Long getClassId(Claims claims) {
+        return claims.get("class_id", Long.class);
+    }
+
+    public AuthorityType getMemberAuthority(Claims claims) {
+        return AuthorityType.valueOf((String) claims.get("member_authority"));
     }
 }
