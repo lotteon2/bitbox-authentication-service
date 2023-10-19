3459df95b71517e2f992e219e9a5b368fa605f4e
wakkpu <ho0524870@gmail.com>
2023-10-19 16:03:40 +0900

    [BUG] cookie

--- src/main/java/com/bitbox/authentication/service/JwtService.java
+++ src/main/java/com/bitbox/authentication/service/JwtService.java
@@ -30,6 +30,7 @@ public class JwtService {
         return ResponseCookie.from("refreshToken", refreshToken)
                 .maxAge(TokenType.REFRESH.getValue())
                 .path("/")
+                .domain("localhost") // TODO : env 등으로 변경?
                 .httpOnly(true)
                 .sameSite("None")
 //                    .secure(true) // TODO : secure(true)시 https에서만 작동.
