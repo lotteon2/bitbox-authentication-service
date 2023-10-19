97d164097551cee43003ddf55fa8222e6fd9859f
wakkpu <ho0524870@gmail.com>
2023-10-19 20:36:21 +0900

    [FIX] getter

--- src/main/java/com/bitbox/authentication/dto/response/LoginResponse.java
+++ src/main/java/com/bitbox/authentication/dto/response/LoginResponse.java
@@ -3,9 +3,11 @@ package com.bitbox.authentication.dto.response;
 import io.github.bitbox.bitbox.enums.AuthorityType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Builder
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginResponse {
