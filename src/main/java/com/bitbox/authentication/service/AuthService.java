be5450592d413e3d0826a04cfc54d695910e8fe8
wakkpu <ho0524870@gmail.com>
2023-10-19 16:03:30 +0900

    [FEAT] authenticate admin

--- src/main/java/com/bitbox/authentication/service/AuthService.java
+++ src/main/java/com/bitbox/authentication/service/AuthService.java
@@ -4,14 +4,21 @@ import com.bitbox.authentication.entity.AuthAdmin;
 import com.bitbox.authentication.exception.NotFoundException;
 import com.bitbox.authentication.repository.AuthAdminRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
     private final AuthAdminRepository authAdminRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public AuthAdmin findAuthAdmin(String adminEmail, String adminPassword) {
-        return authAdminRepository.findByAdminEmailAndAdminPasswordAndDeletedIsFalse(adminEmail, adminPassword).orElseThrow(NotFoundException::new);
+        return authAdminRepository.findByAdminEmailAndAdminPasswordAndDeletedIsFalse(adminEmail, bCryptPasswordEncoder.encode(adminPassword))
+                .orElseThrow(NotFoundException::new); // TODO Exception handling
+    }
+
+    public boolean isFirstLogin(AuthAdmin authAdmin) {
+        return !authAdmin.getCreatedAt().isEqual(authAdmin.getUpdatedAt());
     }
 }
