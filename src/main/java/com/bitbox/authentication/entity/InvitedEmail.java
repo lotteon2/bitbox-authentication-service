22d05b0c66e4f72c96f436bd59cc5cc68fbe2041
wakkpu <ho0524870@gmail.com>
2023-10-20 16:17:31 +0900

    [FEAT] added field

--- src/main/java/com/bitbox/authentication/entity/InvitedEmail.java
+++ src/main/java/com/bitbox/authentication/entity/InvitedEmail.java
@@ -8,6 +8,8 @@ import org.springframework.data.redis.core.index.Indexed;
 
 @Builder
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @RedisHash(value = "invited_email")
 public class InvitedEmail {
     @Id
@@ -17,4 +19,8 @@ public class InvitedEmail {
     private String email;
 
     private Long classId;
+
+    private String classCode;
+
+    private String className;
 }
